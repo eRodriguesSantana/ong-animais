6cb80af840b0d4f6d19568138902cc475a0b0e10
Eduardo R Santana <eduardo.r.santana@hotmail.com>
2024-01-08 20:04:25 -0300

    Correção linkagem

--- lancar_despesa/sub_menu_lancar_despesas.php
+++ lancar_despesa/sub_menu_lancar_despesas.php
@@ -42,7 +42,7 @@ $nivel = $arr['nivelUsuario'];
     <div id="ong" class="container-ong">
       <div class="row">
 
-        <?php include('menu_lateral.php') ?>
+        <?php include('./menu_lateral.php') ?>
         <!--Menu lateral FIM-->
         
         <div class="container" style="margin-top: 10%">
